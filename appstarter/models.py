1b4f73bd05d8247a661031c9a9a72cd1612fc809
john <johnedwardescuyos@gmail.com>
2015-01-02 12:10:26 +0800

    Prototype Modelling for Users, Posts, Groups

--- appstarter/models.py
+++ appstarter/models.py
@@ -2,16 +2,55 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
+
 class User(models.Model):
-    username = models.CharField(max_length=200)
+    username = models.CharField(max_length=50)
     password = models.CharField(max_length=200)
     req_token = models.CharField(max_length=200)
+    log = models.CharField(max_length=5)
+    
     
 class Profile(models.Model):
-    first_name = models.CharField(max_length=200)
-    middle_name = models.CharField(max_length=200)
-    lastname = models.CharField(max_length=200)
+    user_id = models.ForeignKey(User)
+    display_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    position = models.CharField(max_length=30)
+    company_name = models.CharField(max_length=30)
+    mail_address = models.CharField(max_length=30)
 
+    
+class Post(models.Model):
+    user_id = models.ForeignKey(User)
+    share_type = models.CharField(max_length=50)
+    date = models.DateTimeField('date published')
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=500)
+    agrees = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
+    
+    
+class Comment(models.Model):
+    post_id = models.ForeignKey(Post)
+    user_pic_url = models.CharField(max_length=200)
+    user_name = models.CharField(max_length=50)
+    comment_date = models.DateTimeField('date published')
+    comment = models.CharField(max_length=200)
+
+# Groups - A private Collaboration for teams (Page Spec)
+class Group(models.Model):
+    group_name = models.CharField(max_length=50)
+    group_detail = models.CharField(max_length=300)
+    group_created = models.DateTimeField('date published')
+    group_pic = models.CharField(max_length=200)
+    group_users = models.IntegerField(default=0)
+    
+class GroupUser(models.Model):
+    group_id = models.ForeignKey(Post)
+    group_joined_user_id = models.IntegerField(default=0)
+    group_joined_date = models.DateTimeField('date published')
+    
 # Poll App
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -25,6 +64,7 @@ class Question(models.Model):
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
+    
     
 class Choice(models.Model):
     question = models.ForeignKey(Question)
