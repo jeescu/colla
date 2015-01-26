2558b64e68463c32d6d481b96461a75496d48988
john <john.escuyos@98labs.com>
2015-01-26 17:04:43 +0800

    Social login layout

---
+++ appstarter/static/colla/js/login.js
@@ -0,0 +1,16 @@
+function loginSoc() {
+    //init
+}
+
+loginSoc.prototype.facebookLogin = function () {
+
+};
+
+loginSoc.prototype.googleLogin = function () {
+
+};
+
+//send to server
+function verifyAuth(data) {
+    //json
+}
